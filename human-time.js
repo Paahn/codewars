f3df73524d818e86847e06cec524daa22f0186e3
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-04 18:10:09 -0800

    passing all tests

--- human-time.js
+++ human-time.js
@@ -8,3 +8,19 @@ The maximum time never exceeds 359999 (99:59:59)
 
 You can find some examples in the test fixtures.
 */
+
+function humanReadable(seconds) {
+  let sec = 0, min = 0, hour = 0;
+
+  sec = seconds % 60;
+  min = Math.floor(seconds / 60) % 60; // division turns seconds into minutes, which we need to mod by 60 to see the 60 minute format
+  hour = Math.floor(seconds / 3600); 
+  return `${formatTime(hour)}:${formatTime(min)}:${formatTime(sec)}`;
+}
+
+const formatTime = (num) => {
+  if (num < 10) {
+    return `0${num}`;
+  }
+  return String(num);
+}
